33d91b60b0b3eac83bd68ed47d79c2736ac6756a
wither3 <84785651+wither3@users.noreply.github.com>
2025-06-07 16:51:52 +0800

    Add files via upload

--- javanya.js
+++ javanya.js
@@ -9,9 +9,9 @@ contoh: ketik 1 untuk versi bedrock
   const pilihan = prompt(teks);
 
   if (pilihan === "1") {
-    window.location.href = "https://sfile.mobi/7LDVVGP4E8R";
+    window.location.href = "https://sfile.mobi/4qO790jAy4K";
   } else if (pilihan === "2") {
-    window.location.href = "https://sfile.mobi/1OhZ4n0Ehiv";
+    window.location.href = "https://sfile.mobi/3Iu6KT3KcgA";
   } else if (pilihan === "3") {
     window.location.href = "https://chat.whatsapp.com/FZtfxpSyYCN9FyCjWekyZN";
     } else if (pilihan !== null) {
